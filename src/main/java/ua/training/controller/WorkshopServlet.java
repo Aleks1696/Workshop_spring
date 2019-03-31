1ab0fc6b550342ebca4944d309d811856c7a45b5
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-31 19:35:46 +0300

    servlet v1

---
+++ src/main/java/ua/training/controller/WorkshopServlet.java
@@ -0,0 +1,26 @@
+package ua.training.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet()
+public class WorkshopServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doPost(httpServletRequest, httpServletResponse);
+    }
+
+    private void processUser(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
