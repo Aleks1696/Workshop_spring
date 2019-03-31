48cb3a91a72b4917ffda1625ab3d8b703329d8cb
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-31 23:49:12 +0300

    URI constants

---
+++ src/main/java/ua/training/controller/constants/URI.java
@@ -0,0 +1,14 @@
+package ua.training.controller.constants;
+
+public interface URI {
+
+
+
+    String INDEX_JSP = "/index.jsp";
+    String LOGIN_JSP = "/login.jsp";
+    String REGISTRATION_JSP = "/registration.jsp";
+
+    String LOGIN_COMMAND = "/login";
+    String REGISTRATION_COMMAND = "/registration";
+
+}
--- src/main/java/ua/training/controller/constants/URL.java
+++
@@ -1,7 +0,0 @@
-package ua.training.controller.constants;
-
-public interface URL {
-
-    String INDEX_JSP = "/index.jsp";
-    String LOGIN_JSP = "/login.jsp";
-}
