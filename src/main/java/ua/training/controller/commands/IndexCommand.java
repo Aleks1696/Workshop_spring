e06bff991a5b32ea09c6040f84dd2b9b8735d50b
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-31 23:49:44 +0300

    Commands v2

---
+++ src/main/java/ua/training/controller/commands/IndexCommand.java
@@ -0,0 +1,12 @@
+package ua.training.controller.commands;
+
+import ua.training.controller.constants.URI;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IndexCommand implements Command {
+
+    public String execute(HttpServletRequest request) {
+        return URI.INDEX_JSP;
+    }
+}
--- src/main/java/ua/training/controller/commands/RegistrationCommand.java
+++ src/main/java/ua/training/controller/commands/RegistrationCommand.java
@@ -1,12 +1,12 @@
 package ua.training.controller.commands;
 
+import ua.training.controller.constants.URI;
+
 import javax.servlet.http.HttpServletRequest;
 
-public class LoginCommand implements Command {
+public class RegistrationCommand implements Command {
     @Override
     public String execute(HttpServletRequest request) {
-
-
-        return "WEB-INF/customer/customerAccount.jsp";
+        return URI.REGISTRATION_JSP;
     }
 }
