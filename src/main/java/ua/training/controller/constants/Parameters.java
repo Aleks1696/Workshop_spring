5b2fbebb0bab8a1b8cddcd2e9b4b0958b2cbd3af
Aleks1696 <aleks.borodavka@ukr.net>
2019-03-31 19:34:53 +0300

    constants

---
+++ src/main/java/ua/training/controller/constants/Parameters.java
@@ -0,0 +1,8 @@
+package ua.training.controller.constants;
+
+public interface Parameters {
+
+    String LANGUAGE = "language";
+    String USER = "user";
+
+}
---
+++ src/main/java/ua/training/controller/constants/URL.java
@@ -0,0 +1,7 @@
+package ua.training.controller.constants;
+
+public interface URL {
+
+    String INDEX_JSP = "/index.jsp";
+    String LOGIN_JSP = "/login.jsp";
+}
