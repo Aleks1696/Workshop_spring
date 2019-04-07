b826849d97b71a38acd63fafe89da6246bc536ff
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-07 18:24:33 +0300

    user service

--- src/main/java/ua/training/model/service/user/UserService.java
+++ src/main/java/ua/training/model/service/user/UserService.java
@@ -2,8 +2,9 @@ package ua.training.model.service.user;
 
 import ua.training.model.dao.DAOFactory;
 import ua.training.model.entity.User;
+import ua.training.model.exceptions.UserAlreadyExistException;
 
 public interface UserService {
-    DAOFactory FACTORY = DAOFactory.getInstance();
     User logInUser(String login, String password);
+    User createUser(User user) throws UserAlreadyExistException;
 }
--- src/main/java/ua/training/model/service/user/UserServiceImpl.java
+++ src/main/java/ua/training/model/service/user/UserServiceImpl.java
@@ -1,19 +1,27 @@
 package ua.training.model.service.user;
 
+import ua.training.model.dao.DAOFactory;
 import ua.training.model.dao.UserDAO;
 import ua.training.model.entity.User;
+import ua.training.model.types.UserRole;
 
 public class UserServiceImpl implements UserService {
     private UserDAO userDAO;
 
     public UserServiceImpl() {
-        this.userDAO = FACTORY.createUserDAO();
-        System.out.println("User dao object: " + userDAO);
+        this.userDAO = DAOFactory.getInstance().createUserDAO();
+        System.out.println("User dao object from userService: " + userDAO);
     }
 
     @Override
     public User logInUser(String login, String password) {
         User user = userDAO.findByLoginAndPassword(login, password);
         return user;
+    }
+
+    @Override
+    public User createUser(User user) {
+        user.setRole(UserRole.CUSTOMER);
+        return userDAO.create(user);
     }
 }
