239d317dff43426728a15e08a4ca55b13c662305
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-07 18:16:23 +0300

    refactorring

--- src/main/java/ua/training/controller/validation/InputValidation.java
+++ src/main/java/ua/training/controller/validation/InputValidation.java
@@ -1,20 +1,59 @@
 package ua.training.controller.validation;
 
-import ua.training.model.utils.RegexBinder;
+import ua.training.model.entity.User;
+import ua.training.model.utils.MessagesBinder;
+import static ua.training.model.utils.RegexBinder.*;
+
+import java.util.List;
 
 public class InputValidation {
+    private List<String> wrongInputMessages;
 
     //TODO it would be better to check and return login, password validation mistake separately
-    public boolean isLoginAndPasswordValid(String login, String password) {
-        return isLoginValid(login) && isPasswordValid(password);
+    public boolean isLoginAndPasswordValid(String login, String password, List<String> wrongInputMessages) {
+        this.wrongInputMessages = wrongInputMessages;
+        boolean isLoginValid = isParameterValid(login, getProperty("regex.login"),
+                                                MessagesBinder.getProperty("input.wrong.login.format"));
+        boolean isPasswordValid = isParameterValid(password, getProperty("regex.password"),
+                                                   MessagesBinder.getProperty("input.wrong.password.format"));
+        return isLoginValid && isPasswordValid;
     }
 
-    public boolean isLoginValid(String login) {
-        return login != null && login.matches(RegexBinder.getProperty("regex.login"));
+    public boolean isUserValid(User user, List<String> wrongInputMessages) {
+        this.wrongInputMessages = wrongInputMessages;
+
+        boolean isLoginValid = isParameterValid(user.getLogin(), getProperty("regex.login"),
+                MessagesBinder.getProperty("input.wrong.login.format"));
+        boolean isPasswordValid = isParameterValid(user.getPassword(), getProperty("regex.password"),
+                MessagesBinder.getProperty("input.wrong.password.format"));
+        boolean isNameValid = isParameterValid(user.getName(), getProperty("regex.name"),
+                MessagesBinder.getProperty("input.wrong.name.format"));
+        boolean isName_uaValid = isParameterValid(user.getName_ua(), getProperty("regex.name_ua"),
+                MessagesBinder.getProperty("input.wrong.name_ua.format"));
+        boolean isSurnameValid = isParameterValid(user.getSurname(), getProperty("regex.surname"),
+                MessagesBinder.getProperty("input.wrong.surname.format"));
+        boolean isSurname_uaValid = isParameterValid(user.getSurname_ua(), getProperty("regex.surname_ua"),
+                MessagesBinder.getProperty("input.wrong.surname_ua.format"));
+        boolean isEmailValid = isParameterValid(user.getEmail(), getProperty("regex.email"),
+                MessagesBinder.getProperty("input.wrong.email.format"));
+        boolean isPhoneNumberValid = isParameterValid(user.getPhoneNumber(), getProperty("regex.phoneNumber"),
+                MessagesBinder.getProperty("input.wrong.phoneNumber.format"));
+
+        return isLoginValid && isPasswordValid && isNameValid && isName_uaValid && isSurnameValid &&
+                isSurname_uaValid && isEmailValid && isPhoneNumberValid;
     }
 
-    public boolean isPasswordValid(String password) {
-        return password != null && password.matches(RegexBinder.getProperty("regex.password"));
+    private boolean isParameterValid(String parameter, String regex, String errorMessage) {
+        if (parameter == null || !(parameter.matches(regex))) {
+            wrongInputMessages.add(errorMessage);
+            return false;
+        } else {
+            return true;
+        }
     }
+
+
+
+
 
 }
