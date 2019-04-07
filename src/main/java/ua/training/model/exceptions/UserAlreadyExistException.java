ce03d738bc89dd00336a0b16ddf9b9ddd0df99c1
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-07 18:22:42 +0300

    new exception if user in found in db

---
+++ src/main/java/ua/training/model/exceptions/UserAlreadyExistException.java
@@ -0,0 +1,11 @@
+package ua.training.model.exceptions;
+
+public class UserAlreadyExistException extends RuntimeException {
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
