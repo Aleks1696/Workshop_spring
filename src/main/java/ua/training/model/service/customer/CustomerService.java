aca94bc30cf5e2dafe8e6d12c68770c731fed6e4
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-07 18:23:58 +0300

    customer service

--- src/main/java/ua/training/model/service/customer/CustomerService.java
+++ src/main/java/ua/training/model/service/customer/CustomerService.java
@@ -1,4 +1,10 @@
 package ua.training.model.service.customer;
 
+import ua.training.model.entity.Request;
+import ua.training.model.entity.User;
+
+import java.util.List;
+
 public interface CustomerService {
+    List<Request> getActiveRequests(User user);
 }
--- src/main/java/ua/training/model/service/customer/CustomerServiceImpl.java
+++ src/main/java/ua/training/model/service/customer/CustomerServiceImpl.java
@@ -1,5 +1,23 @@
 package ua.training.model.service.customer;
 
-public class CustomerServiceImpl implements CustomerService {
+import ua.training.model.dao.DAOFactory;
+import ua.training.model.dao.RequestDAO;
+import ua.training.model.entity.Request;
+import ua.training.model.entity.User;
 
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    private RequestDAO requestDAO;
+
+    public CustomerServiceImpl() {
+        this.requestDAO = DAOFactory.getInstance().createRequestDAO();
+        System.out.println("User dao object from userService: " + requestDAO);
+    }
+
+    @Override
+    public List<Request> getActiveRequests(User user) {
+        return requestDAO.findAllByUserId(user.getId());
+
+    }
 }
