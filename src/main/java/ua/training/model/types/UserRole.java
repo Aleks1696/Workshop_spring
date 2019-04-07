c5a4eb3cbbcf9159a6ee736d47ab60b6024ab434
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-07 18:26:50 +0300

    minor changes

--- src/main/java/ua/training/model/types/UserRole.java
+++ src/main/java/ua/training/model/types/UserRole.java
@@ -1,12 +1,18 @@
 package ua.training.model.types;
 
+import ua.training.model.utils.URIBinder;
+
 public enum UserRole {
-    CUSTOMER("/customer"), MANAGER("/manager"), MASTER("/master");
+    CUSTOMER("/customer", URIBinder.getProperty("path.customer.account")),
+    MANAGER("/manager", URIBinder.getProperty("path.manager.account")),
+    MASTER("/master", URIBinder.getProperty("path.master.account"));
 
     private String allowedPath;
+    private String basePage;
 
-    UserRole(String path) {
+    UserRole(String path, String page) {
         allowedPath = path;
+        basePage = page;
     }
 
     public static UserRole getRole(String role) {
@@ -16,5 +22,9 @@ public enum UserRole {
 
     public String getAllowedPath() {
         return allowedPath;
+    }
+
+    public String getBasePath() {
+        return basePage;
     }
 }
