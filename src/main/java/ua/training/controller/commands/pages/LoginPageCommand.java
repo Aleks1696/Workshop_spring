860bcf5b001d0f37c7552d4a2019aeba5803ebdb
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-11 22:59:34 +0300

    Do not let user login twice. If user is logged then return basepage every time login page called

--- src/main/java/ua/training/controller/commands/pages/LoginPageCommand.java
+++ src/main/java/ua/training/controller/commands/pages/LoginPageCommand.java
@@ -1,14 +1,26 @@
 package ua.training.controller.commands.pages;
 
 import ua.training.controller.commands.Command;
+import ua.training.model.entity.User;
+import ua.training.model.types.UserRole;
+import ua.training.model.utils.AttributesBinder;
 import ua.training.model.utils.URIBinder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 public class LoginPageCommand implements Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        Optional<User> user =
+                Optional.ofNullable((User) session.getAttribute(AttributesBinder.getProperty("parameter.user")));
+        if (user.isPresent()) {
+            UserRole role = user.get().getRole();
+            return URIBinder.getProperty("redirect") + role.getBasePath();
+        }
         return URIBinder.getProperty("jsp.login");
     }
 }
