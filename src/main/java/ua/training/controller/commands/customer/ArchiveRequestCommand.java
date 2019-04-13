5b120b3afb24f228d153be037cb48766cd2cf6db
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-13 04:07:31 +0300

    by archiving request user moves it to archive table in db

---
+++ src/main/java/ua/training/controller/commands/customer/ArchiveRequestCommand.java
@@ -0,0 +1,33 @@
+package ua.training.controller.commands.customer;
+
+import ua.training.controller.commands.Command;
+import ua.training.model.entity.Request;
+import ua.training.model.service.customer.CustomerService;
+import ua.training.model.service.customer.CustomerServiceImpl;
+import ua.training.model.utils.AttributesBinder;
+import ua.training.model.utils.URIBinder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+public class ArchiveRequestCommand implements Command {
+    private CustomerService customerService;
+
+    public ArchiveRequestCommand() {
+        this.customerService = new CustomerServiceImpl();
+    }
+
+    @Override
+    public String execute(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+        Request request = new Request();
+        request.setId(Integer.valueOf(httpRequest.getParameter(AttributesBinder.getProperty("parameter.id"))));
+        try {
+            customerService.archiveRequest(request);
+        } catch (SQLException e) {
+            log.error("Failed moving from request table to archive during transaction");
+            e.printStackTrace();
+        }
+        return URIBinder.getProperty("redirect") + URIBinder.getProperty("path.customer.notifications");
+    }
+}
