4ec7e413d5cb298f3f1ff3972de688440faf38b0
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-13 04:13:23 +0300

    servlet context is connected

--- src/main/java/ua/training/controller/commands/LogoutCommand.java
+++ src/main/java/ua/training/controller/commands/LogoutCommand.java
@@ -1,17 +1,28 @@
 package ua.training.controller.commands;
 
-import ua.training.model.utils.AttributesBinder;
+import ua.training.model.entity.User;
+import static ua.training.model.utils.AttributesBinder.*;
 import ua.training.model.utils.URIBinder;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LogoutCommand implements Command {
+
     @Override
+    @SuppressWarnings("unchecked")
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
-        session.removeAttribute(AttributesBinder.getProperty("parameter.user"));
+        ServletContext servletContext = session.getServletContext();
+        User user = (User) session.getAttribute(getProperty("parameter.user"));
+        Map<User, HttpSession> loggedUsers =
+                (HashMap<User, HttpSession>)servletContext.getAttribute(getProperty("attribute.servlet.context.logged.users"));
+        session.removeAttribute(getProperty("parameter.user"));
+        loggedUsers.remove(user);
         return URIBinder.getProperty("redirect") + URIBinder.getProperty("path.index");
     }
 }
