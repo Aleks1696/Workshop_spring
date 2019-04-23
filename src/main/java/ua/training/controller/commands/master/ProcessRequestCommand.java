4ce1dcee9f1e20231442f5137d3227aea0e43c7d
Aleks1696 <aleks.borodavka@ukr.net>
2019-04-23 14:52:55 +0300

    refactoring

--- src/main/java/ua/training/controller/commands/master/ProcessRequestCommand.java
+++ src/main/java/ua/training/controller/commands/master/ProcessRequestCommand.java
@@ -3,7 +3,6 @@ package ua.training.controller.commands.master;
 import ua.training.controller.commands.Command;
 import ua.training.model.entity.*;
 import ua.training.model.service.master.*;
-import ua.training.model.types.RequestStatus;
 import ua.training.model.utils.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,20 +21,19 @@ public class ProcessRequestCommand implements Command {
     public String execute(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
         log.info("Try to accept requests by master");
         Request request = new Request();
-        setRelatedParameters(httpRequest, request);
+        setParameters(httpRequest, request);
         try {
             masterService.processRequest(request);
         } catch (Exception e) {
-            log.error("Error accepting request by master");
+            log.error("Error accepting request by master", e);
         }
         return URIBinder.getProperty("redirect") + URIBinder.getProperty("path.master.active.request");
     }
 
-    private void setRelatedParameters(HttpServletRequest httpRequest, Request request) {
+    private void setParameters(HttpServletRequest httpRequest, Request request) {
         HttpSession session = httpRequest.getSession();
-        request.setId(Integer.valueOf(httpRequest.getParameter(getProperty(("parameter.id")))));
+        request.setId(Integer.valueOf(httpRequest.getParameter(getProperty("parameter.id"))));
         User currentMaster = (User) session.getAttribute(AttributesBinder.getProperty("parameter.user"));
         request.setMaster_id(currentMaster.getId());
-        request.setStatus(RequestStatus.IN_PROCESS);
     }
 }
